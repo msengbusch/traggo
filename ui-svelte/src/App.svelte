3feef3286f60c6515dea6f3e378795193008ee8d
msengbusch <marek.sengbusch@flg-online.de>
2023-11-02 10:33:04 +0100

    feat: vite svelte tailwindcss setup

---
+++ ui-svelte/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<main>
+  <h1 class="text-3xl font-bold underline ">
+    Hello world!
+  </h1>
+</main>
