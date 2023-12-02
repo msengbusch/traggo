bd984af6f7f6bd385c4b84e2c068b76549d2ed74
msengbusch <marek.sengbusch@flg-online.de>
2023-12-02 12:50:18 +0100

    chore: add svelte 4

--- ui-svelte/vite.config.ts
+++ ui-svelte/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  //build: {
-  //  outDir: '../dist'
-  //},
-  //root: './src',
   plugins: [svelte()],
 })
