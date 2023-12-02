64345cd7137e723a1f7ead152e72fc6af2e5047f
msengbusch <marek.sengbusch@flg-online.de>
2023-12-02 14:49:14 +0100

    feat: add tailwindcss and postcss

--- ui-svelte/src/App.svelte
+++ ui-svelte/src/App.svelte
@@ -2,7 +2,7 @@
     import Counter from "./lib/Counter.svelte";
 </script>
 
-<main>
+<main class="bg-amber-300">
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
 
@@ -12,7 +12,7 @@
   </div>
   <h1>Vite + Svelte</h1>
 
-  <div class="card">
+  <div class="text-cyan-300">
     <Counter></Counter>
   </div>
 
@@ -20,13 +20,7 @@
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
+  <p>
     Click on the Vite and Svelte logos to learn more
   </p>
 </main>
-
-<style>
-  .read-the-docs {
-    color: #888;
-  }
-</style>
