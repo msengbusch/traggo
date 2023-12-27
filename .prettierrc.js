e3fc67eaecb8703435369506bdbeef8fba1667fa
mseng <sengbusch.marek@gmail.com>
2023-12-27 10:41:56 +0100

    style: format with prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,12 @@
 export default {
-    trailingComma: "none",
-    plugins: [
-        "prettier-plugin-svelte"
-    ],
-    overrides: [
-        {
-            files: "*.svelte",
-            options: {
-                parser: "svelte"
-            }
-        }
-    ]
-}
+	trailingComma: "none",
+	plugins: ["prettier-plugin-svelte"],
+	overrides: [
+		{
+			files: "*.svelte",
+			options: {
+				parser: "svelte"
+			}
+		}
+	]
+};
