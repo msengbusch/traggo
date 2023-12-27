7036fbc5f110ae0588309113057087479d3cc958
mseng <sengbusch.marek@gmail.com>
2023-12-27 14:13:10 +0100

    ci: disable testing as long as vitest hangs

--- web/src/dummy.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-
-// TOOD: Remove. Only needed so that CI dosen't fail complaining that there are no tests
-test("dummy", () => {
-  expect(true).toBe(true);
-});
