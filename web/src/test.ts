863657c17320cdc2a02db4ed99cde3ab7ad2d60c
mseng <sengbusch.marek@gmail.com>
2023-12-27 14:01:28 +0100

    ci: fix test failing

---
+++ web/src/test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+
+// TOOD: Remove. Only needed so that CI dosen't fail complaining that there are no tests
+test('dummy', () => {
+  expect(true).toBe(true)
+})
