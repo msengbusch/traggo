f420908f65c251104435ade415349322f3f06d57
mseng <sengbusch.marek@gmail.com>
2023-12-27 13:59:30 +0100

    style: prettier

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,12 +1,7 @@
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,ts,svelte}"
-  ],
-  plugins: [
-    daisyui
-  ]
-}
-
+  content: ["./src/**/*.{html,js,ts,svelte}"],
+  plugins: [daisyui]
+};
