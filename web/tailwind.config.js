0a2d77b0b1e588c5dce63a93520b4c59e10108b5
mseng <sengbusch.marek@gmail.com>
2023-12-27 13:42:16 +0100

    chore: add daisyui

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,12 @@
+import daisyui from "daisyui"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{html,js,ts,svelte}"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+    daisyui
+  ]
 }
 
