25961a19e93e3f1ff120fb5410d548e245e8bd87
msengbusch <marek.sengbusch@flg-online.de>
2024-01-02 22:43:05 +0100

    feat: add user component

---
+++ web/src/lib/nav/Sidebar.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<div class="drawer lg:drawer-open">
+  <input id="sidebar" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    Hello World!
+    <label for="sidebar" class="btn btn-primary drawer-button">Open</label>
+    <slot />
+  </div>
+  <div class="drawer-side">
+    <label for="sidebar" aria-label="close sidebar" class="drawer-overlay"></label>
+    <div class="p-4 w-80 min-h-full bg-base-200 flex flex-col">
+      <a href="https://github.com/traggo" class="text-3xl self-center text-primary"> traggo </a>
+      <div class="divider"></div>
+      <ul class="">
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Item 2</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
