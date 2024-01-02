085b445537a0b12f3573333ef146399b100ff572
msengbusch <marek.sengbusch@flg-online.de>
2024-01-02 22:43:05 +0100

    feat: add user component

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,53 +1,15 @@
 <script>
-  import Header from "./Header.svelte";
   import "./styles.css";
+  import Sidebar from "$lib/nav/Sidebar.svelte";
+  import Header from "$lib/nav/Header.svelte";
+  import User from "$lib/components/User.svelte";
 </script>
 
-<div class="app">
-  <Header />
+<User></User>
 
-  <main>
-    <slot />
-  </main>
-
-  <footer>
-    <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-  </footer>
+<!-- <div class="app">
+  <Sidebar>
+    <slot></slot>
+  </Sidebar>
 </div>
-
-<style>
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 64rem;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
-  }
-</style>
+ -->
