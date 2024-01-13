835ef0582f0728a0bc08ddd068e74ea635d17205
msengbusch <marek.sengbusch@flg-online.de>
2024-01-13 14:19:32 +0100

    feat: add sidebar

---
+++ web/src/lib/nav/Navigation.svelte
@@ -0,0 +1,64 @@
+<script>
+</script>
+
+{#snippet group({ header, entries })}
+  <div class="divider">{header}</div>
+  <ul>
+    {#each entries as entry}
+      <li>
+        <button class="btn btn-sm btn-ghost min-w-full justify-start">
+          <span class="material-icons">
+            {entry.icon}
+          </span>
+          {entry.name}
+        </button>
+      </li>
+    {/each}
+  </ul>
+{/snippet}
+
+<div>
+  <nav>
+    <!-- <label for="sidebar" class="btn btn-primary drawer-button">Open</label> -->
+  </nav>
+  <aside class="drawer lg:drawer-open">
+    <input id="sidebar" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <slot />
+    </div>
+    <div class="drawer-side">
+      <div class="flex flex-col p-4 w-80 min-h-full bg-base-200">
+        <a href="https://github.com/traggo" class="text-3xl self-center text-primary"> traggo </a>
+        {@render group({
+          header: "Dashboards",
+          entries: [
+            { name: "Default", icon: "dashboard" },
+            { name: "Manager", icon: "list_alt" }
+          ]
+        })}
+        {@render group({
+          header: "Timesheet",
+          entries: [
+            { name: "List", icon: "timeline" },
+            { name: "Calendar", icon: "calendar_today" }
+          ]
+        })}
+        {@render group({
+          header: "User",
+          entries: [
+            { name: "Tags", icon: "label" },
+            { name: "Devices", icon: "devices" },
+            { name: "Settings", icon: "settings" }
+          ]
+        })}
+        {@render group({
+          header: "Admin",
+          entries: [
+            { name: "Users", icon: "supervisor_account" },
+            { name: "Settings", icon: "tune" }
+          ]
+        })}
+      </div>
+    </div>
+  </aside>
+</div>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script>
+  import Navigation from "$lib/nav/Navigation.svelte";
   import "./styles.css";
-  import Sidebar from "$lib/nav/Sidebar.svelte";
-  import Header from "$lib/nav/Header.svelte";
-  import User from "$lib/components/User.svelte";
 </script>
 
-<User></User>
-
-<!-- <div class="app">
-  <Sidebar>
-    <slot></slot>
-  </Sidebar>
+<div class="app">
+  <Navigation>
+    Content
+    <slot />
+  </Navigation>
 </div>
- -->
